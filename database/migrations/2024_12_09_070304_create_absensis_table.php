f234c58a1f7b1bdaa6717b7b1b18f762782bda8f
ezy123456 <2172018@maranatha.ac.id>
2024-12-11 20:49:08 +0700

    add migration

---
+++ database/migrations/2024_12_09_070304_create_absensis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absensis', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_absensi');
+            $table->time('time_in');
+            $table->time('time_out');
+            $table->string('status');
+            $table->foreignId('employee_id')->references('id')->on('employees')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absensis');
+    }
+};
